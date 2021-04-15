921f09b7d0f52f77c586f3de9e493051f0577b52
thelokys <ricardo.guidetti@hotmail.com>
2021-04-15 20:17:13 -0300

    added ignews projetct

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+
+import { SignInButton } from '../SignInButtton'
+import styles from './styles.module.scss'
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news"/>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a href="">Posts</a>
+        </nav>
+        <SignInButton/>
+      </div>
+    </header>
+  );
+}
