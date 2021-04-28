6751e816e48bc7fb916bc010d4b4cc8b32f5ca96
thelokys <ricardo.guidetti@hotmail.com>
2021-04-28 19:33:18 -0300

    added ignews

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -31,9 +31,18 @@ export default function Post({ post }: PostsProps) {
   )
 }
 
-export const getServerSideProps:GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession();
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req });
   const { slug } = params;
+  console.log(session)
+  if(!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
